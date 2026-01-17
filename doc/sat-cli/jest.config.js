753815f8d135be25020def4e49b5f5bc398b1bad
ki11e6 <sharath.surendran.m@gmail.com>
2026-01-17 11:09:17 +0530

    chore: add Jest testing infrastructure
    - Install jest, @types/jest, ts-jest as dev dependencies
    - Add jest.config.js with ts-jest preset
    - Configure test matching and coverage paths

---
+++ doc/sat-cli/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.json' }]
+  },
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.test.ts'],
+  coverageDirectory: 'coverage',
+  verbose: true
+};
